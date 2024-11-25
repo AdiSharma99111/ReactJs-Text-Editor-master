9a8682e621c7fd3c65434eaa2f40f45ebeb7da34
AdiSharma99111 <147161471+AdiSharma99111@users.noreply.github.com>
2024-11-25 13:40:53 +0530

    Initial commit

---
+++ src/components/TextAnalytics.jsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+// const TextAnalytics = ({ text }) => {
+//   const wordCount = text.split(/\s+/).filter(Boolean).length;
+//   const charCount = text.length;
+//   const readingTime = Math.round(wordCount / 200); 
+
+//   return (
+//     <div>
+//       <p>Word Count: {wordCount}</p>
+//       <p>Character Count: {charCount}</p>
+//       <p>Estimated Reading Time: {readingTime} minute(s)</p>
+//     </div>
+//   );
+// };
+
+// export default TextAnalytics;
